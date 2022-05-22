9a525a546aab0a6eda5b806dd92a6bff54998ab6
gerlecztamas <gerlecztamas@gmail.com>
2022-05-22 22:32:38 +0200

    list subjects

--- src/main/java/business/Methods.java
+++ src/main/java/business/Methods.java
@@ -7,14 +7,31 @@ import java.util.ArrayList;
 
 public class Methods {
 
-    public static void felvehetoKiiratas (ArrayList<FelvehetoTantargy> targyak) {
-        for(FelvehetoTantargy targy: targyak) {
+    public static void felvehetoKiiratas (ArrayList<FelvehetoTantargy> felvehetoTargyak) {
+        for(FelvehetoTantargy targy: felvehetoTargyak) {
+            boolean bool = false;
             /*for(int i = 0; i < Fio.felvettTantargyak.size(); i++) {
                 Fio.felvettTantargyak.get(i).getNev()
             }*/
             //meg kéne nézni hogy fel van-e már véve / teljesítve van-e, ha nem akkor kiiratni!
-            System.out.println(targy);
+            for(Tantargy targy2 : Fio.felvettTantargyak){
+                if(targy.getNev().equals(targy2.getNev())){
+                    bool = true;
+                    break;
+                }
+                for(TeljesitettTantargy targy3 : Fio.teljesitettTantargyak) {
+                    if(targy.getNev().equals(targy3.getNev())){
+                        bool = true;
+                        break;
+                    }
+                }
+            }
+            if(bool == false){
+                System.out.println(targy);
+            }
+
         }
+
     }
 
     public static void felvettKiiratas (ArrayList<Tantargy> targyak) {
